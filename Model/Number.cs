525da1d5335610545996af28debe80892afc5363
mike <mikolajpaziewskialfa@gmail.com>
2023-06-03 17:05:04 +0200

    Model Number created

---
+++ Model/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mobile_systems_project.Model
+{
+    public class Number
+    {
+        public string NumericValue { get; set; }
+        public string TextualValue { get; set; }
+    }
+}
